71dc62dc2771d1ae90d83fa20bf972195479491c
xDenieru <evandar-elda@hotmail.com>
2014-06-13 01:15:33 +0200

    Finished core mechanics
    Still need to add upgrades.

--- game.js
+++ game.js
@@ -1,32 +1,42 @@
 //variables
 var s = { //stats
 	oreoTotal: 0, //Total Oreos
-	mpx: 1,
-	rate: 1,
-	ops: a.add*mpx
+	mpx: 1, //Multiplier
+	rate: 1, //Rate, can be halved for half rate of earning oreos
+	add:0, //Amount of oreos to add per second automaticly
+	ops: 0 //Oreos per second
 }
-var a = { //automatic
-	add:0
+var o = { //Ops upgrades
+	
+}
+var m = { //Mpx upgrades
+	
 }
 setInterval(updateLayout,10);
+setInterval(addOreos,100);
 //functions
 function updateLayout () {
-	money.total+=add;
-	$('#oreosTotal').html(s.oreoTotal+" Oreos");
+	s.ops = s.add*s.mpx*s.rate;
+	$('#oreosTotal').html(Math.floor(s.oreoTotal)+" Oreos");
 	$('#mpx').html("Your multiplier is "+s.mpx+"!");
 	$('#ops').html("You are currently earning "+s.ops+" Oreos per second!");
 }
-function updateStats () {
-	
+function addOreos() {
+	s.oreoTotal+=((s.add*s.mpx*s.rate)/10)
 }
-setInterval('updateStats',1000)
 //setInterval(function(){console.log("This works!")},1000);
 
 //jQuery code
 $(document).ready(function(){
-$('#clicker').click(function() {
-	money.total+=(s.mpx*s.rate); //adds money by clicking.
-})})
+$('#clicker')
+	.mousedown(function() {
+	$('#clicker').css({"height":"215px","width":"215px"}) //reduces size
+	})
+	.mouseup(function() {
+		s.oreoTotal+=(s.mpx*s.rate); //adds money by clicking.
+		$('#clicker').css({"height":"220px","width":"220px"}) //increases size
+	})
+});
 
 /*
 For hver ny upgrade man køber:
