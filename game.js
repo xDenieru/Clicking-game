d7dfd610242a6d66c050a84c824608df3e4197cb
xDenieru <evandar-elda@hotmail.com>
2014-06-13 20:43:00 +0200

    - jQuery library is now hosted locally - Added my first upgrade - Updated Readme

--- game.js
+++ game.js
@@ -1,4 +1,13 @@
-//variables
+//##############
+//####Timers####
+//##############
+setInterval(updateLayout,10);
+setInterval(addOreos,100);
+
+
+//##############
+//##Variables###
+//##############
 var s = { //stats
 	oreoTotal: 0, //Total Oreos
 	mpx: 1, //Multiplier
@@ -6,15 +15,17 @@ var s = { //stats
 	add:0, //Amount of oreos to add per second automaticly
 	ops: 0 //Oreos per second
 }
-var o = { //Ops upgrades
-	
-}
-var m = { //Mpx upgrades
-	
-}
-setInterval(updateLayout,10);
-setInterval(addOreos,100);
-//functions
+
+
+//##############
+//#####Lists####
+//##############
+//Upgrade cost modifier,upgrade cost,amount of ops to add,name,description
+var cookieMonster = new opsUpg(1,100,5,"Cookie Monster","COOKIEZ!<br>This upgrade increases your OPS by 5.")
+
+//##############
+//##Functions###
+//##############
 function updateLayout () {
 	s.ops = s.add*s.mpx*s.rate;
 	$('#oreosTotal').html(Math.floor(s.oreoTotal)+" Oreos");
@@ -24,9 +35,17 @@ function updateLayout () {
 function addOreos() {
 	s.oreoTotal+=((s.add*s.mpx*s.rate)/10)
 }
-//setInterval(function(){console.log("This works!")},1000);
+function opsUpg (increase,cost,add,name,description) {
+	this.increase = increase;
+	this.cost=cost*increase;
+	this.add=add;
+	this.name=name;
+	this.description=description;
+}
 
-//jQuery code
+//##############
+//#General Code#
+//##############
 $(document).ready(function(){
 $('#clicker')
 	.mousedown(function() {
@@ -36,10 +55,17 @@ $('#clicker')
 		s.oreoTotal+=(s.mpx*s.rate); //adds money by clicking.
 		$('#clicker').css({"height":"220px","width":"220px"}) //increases size
 	})
+//Cookiemonster
+$('#opsUpg').html("<button type='button' onclick='if(s.oreoTotal>=cookieMonster.cost){s.oreoTotal-=cookieMonster.cost;s.add+=cookieMonster.add;cookieMonster.increase+=0.1}'>"+"Purchase "+cookieMonster.name+""+"</button>");
+//$('#opsUpg').
 });
 
 /*
-For hver ny upgrade man køber:
-	add+=upgrade // normale upgrades
-	mpx+=upgrade //multiplier
+====HUSKELISTE====
+- For hver ny upgrade man køber:
+	- add+=upgrade // normale upgrades
+	- mpx+=upgrade //multiplier
+- Funktioner til at gøre upgrade overblikket nemmere.
+
+//setInterval(function(){console.log("This works!")},1000);
 */
